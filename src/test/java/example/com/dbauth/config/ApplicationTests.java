7780e136d62814a6f494b975c8c550423c50921e
Dariusz Urbanek <dariuszurbanek@gmail.com>
2019-06-22 20:50:08 +0100

    test fix

--- src/test/java/example/com/dbauth/config/ApplicationTests.java
+++ src/test/java/example/com/dbauth/config/ApplicationTests.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -17,7 +18,11 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -32,44 +37,45 @@ public class ApplicationTests {
 //	@Qualifier("EmployeeDaoImpl")
 	private EmployeeDAO employeeDAO;
 	
-	
-//	 @Autowired
-//	 private MockMvc mockMvc;
 
-	// @Test
-	// public void loginWithValidUserThenAuthenticated() throws Exception {
-	// FormLoginRequestBuilder login = formLogin()
-	// .user("user")
-	// .password("password");
-	//
-	// mockMvc.perform(login)
-	// .andExpect(authenticated().withUsername("user"));
-	// }
-	//
-	// @Test
-	// public void loginWithInvalidUserThenUnauthenticated() throws Exception {
-	// FormLoginRequestBuilder login = formLogin()
-	// .user("invalid")
-	// .password("invalidpassword");
-	//
-	// mockMvc.perform(login)
-	// .andExpect(unauthenticated());
-	// }
-	//
-	// @Test
-	// public void accessUnsecuredResourceThenOk() throws Exception {
-	// mockMvc.perform(get("/"))
-	// .andExpect(status().isOk());
-	// }
-	//
-	// @Test
-	// public void accessSecuredResourceUnauthenticatedThenRedirectsToLogin() throws
-	// Exception {
-	// mockMvc.perform(get("/hello"))
-	// .andExpect(status().is3xxRedirection())
-	// .andExpect(redirectedUrlPattern("**/login"));
-	// }
-	//
+
+	 @Autowired
+	 private MockMvc mockMvc;
+
+	 @Test
+	 public void loginWithValidUserThenAuthenticated() throws Exception {
+	 SecurityMockMvcRequestBuilders.FormLoginRequestBuilder login = formLogin()
+	 .user("user")
+	 .password("password");
+
+	 mockMvc.perform(login)
+	 .andExpect(authenticated().withUsername("user"));
+	 }
+
+	 @Test
+	 public void loginWithInvalidUserThenUnauthenticated() throws Exception {
+	 SecurityMockMvcRequestBuilders.FormLoginRequestBuilder login = formLogin()
+	 .user("invalid")
+	 .password("invalidpassword");
+
+	 mockMvc.perform(login)
+	 .andExpect(unauthenticated());
+	 }
+
+	 @Test
+	 public void accessUnsecuredResourceThenOk() throws Exception {
+	 mockMvc.perform(get("/"))
+	 .andExpect(status().isOk());
+	 }
+
+//	 @Test
+//	 public void accessSecuredResourceUnauthenticatedThenRedirectsToLogin() throws
+//	 Exception {
+//	 mockMvc.perform(get("/hello"))
+//	 .andExpect(status().is3xxRedirection())
+//	 .andExpect(redirectedUrlPattern("**/login"));
+//	 }
+//
 //	 @Test
 //	 @WithMockUser
 //	 public void accessSecuredResourceAuthenticatedThenOk() throws Exception {
